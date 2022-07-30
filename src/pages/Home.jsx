f1a6b8c5bd8618a3a091d7bfc6c0defa0d7ff156
Viktor Melnyk <viktorzt2111@gmail.com>
2022-07-30 19:29:49 +0300

    Basic structure our App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Popular from '../components/Popular';
+import Veggie from '../components/Veggie';
+
+function Home() {
+	return (
+		<>
+			<Popular />
+			<Veggie />
+		</>
+	)
+}
+
+export default Home
---
+++ src/pages/Sheets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Home from './Home';
+
+export const Sheets = () => {
+	return (
+		<div>
+			<Home />
+		</div>
+	)
+}
